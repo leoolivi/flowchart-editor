ee850da4b492bf189943e3b4bc84b5a43f92d02c
leoolivi <olivierileo19@gmail.com>
2025-12-23 23:28:40 +0100

    fix: removed unused import (caused deploy error)

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useRef, useState } from 'react';
+import { useCallback, useRef, useState } from 'react';
 import { ReactFlow, addEdge, Background, useNodesState, useEdgesState, reconnectEdge } from '@xyflow/react';
 import '@xyflow/react/dist/style.css';
 import NodeGraph from './models/NodeGraph';
